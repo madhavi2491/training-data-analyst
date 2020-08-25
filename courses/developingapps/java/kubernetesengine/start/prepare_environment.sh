9618c08438a4f84ad20ae19ada8621aa23eaeb8c
madhavi0891 <66946576+madhavi0891@users.noreply.github.com>
2020-08-26 00:23:04 +0530

    Update prepare_environment.sh

--- courses/developingapps/java/kubernetesengine/start/prepare_environment.sh
+++ courses/developingapps/java/kubernetesengine/start/prepare_environment.sh
@@ -18,7 +18,7 @@ echo "Making bucket: gs://$DEVSHELL_PROJECT_ID-media"
 aws s3 mb gs://$DEVSHELL_PROJECT_ID-media
 
 echo "Exporting GCLOUD_PROJECT and GCLOUD_BUCKET"
-export GCLOUD_PROJECT=myprojectid
+export GCLOUD_PROJECT=$DEVSHELL_PROJECT_ID
 export GCLOUD_BUCKET=$DEVSHELL_PROJECT_ID-media
 
 echo "Installing dependencies"
@@ -34,4 +34,4 @@ echo "Creating Cloud Spanner Instance, Database, and Table"
 gcloud spanner instances create quiz-instance --config=regional-us-central1 --description="Quiz instance" --nodes=1
 gcloud spanner databases create quiz-database --instance quiz-instance --ddl "CREATE TABLE Feedback ( feedbackId STRING(100) NOT NULL, email STRING(100), quiz STRING(20), feedback STRING(MAX), rating INT64, score FLOAT64, timestamp INT64 ) PRIMARY KEY (feedbackId);"
 
-echo "Project ID: myprojectid"
+echo "Project ID: $DEVSHELL_PROJECT_ID"
